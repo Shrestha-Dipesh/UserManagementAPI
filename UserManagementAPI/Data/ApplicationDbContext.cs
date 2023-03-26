ec7777c055c86ee5c7f697f9ed67e94d55ab3b2b
shrestha-dipesh <shrestha.dipesh1999@gmailcom>
2023-03-26 11:12:40 +0545

    Created user model and added table to the database

---
+++ UserManagementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ UserManagementAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string? Address { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
